d3fb8232b0002a68f5de54b995f36905df471995
Dexter1245639 <dexter1245639@gmail.com>
2024-11-29 15:10:35 +0100

    commit via intellij

--- app/src/main/java/com/example/minutnik_android/fragmentKonfiguracja.kt
+++ app/src/main/java/com/example/minutnik_android/fragmentKonfiguracja.kt
@@ -12,10 +12,7 @@ class fragmentKonfiguracja : Fragment() {
     private var minuty = 0
     private var sekundy = 0
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_konfiguracja, container, false)
 
         val minutyTextView = view.findViewById<TextView>(R.id.tvMinuty)
@@ -29,7 +26,7 @@ class fragmentKonfiguracja : Fragment() {
         val minutnik = view.findViewById<Button>(R.id.minutnik)
 
         plusMinuty.setOnClickListener {
-            minuty++
+            if(minuty < 59) minuty++
             minutyTextView.text = minuty.toString()
         }
 
--- app/src/main/java/com/example/minutnik_android/fragmentMinutnik.kt
+++ app/src/main/java/com/example/minutnik_android/fragmentMinutnik.kt
@@ -1,22 +1,16 @@
 package com.example.minutnik_android
 
-import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
-import android.util.Log
 
 class fragmentMinutnik : Fragment() {
-    private var minutnik: CountDownTimer? = null
-    private var isRunning = false
-    private var pozostalyCzas: Long = 0L
+    private var timer: Timer? = null
     private var czasStartu: Long = 0L
-    private var dzwiekKonca: MediaPlayer? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_minutnik, container, false)
@@ -26,33 +20,66 @@ class fragmentMinutnik : Fragment() {
         val buttonStop = view.findViewById<Button>(R.id.buttonStop)
         val buttonPauza = view.findViewById<Button>(R.id.buttonPauza)
         val konfiguracja = view.findViewById<Button>(R.id.buttonKonfiguracja)
+        val buttonDodajSekundaJednosci = view.findViewById<Button>(R.id.buttonDodajSekundaJednosci)
+        val buttonDodajSekundaDziesiatek = view.findViewById<Button>(R.id.buttonDodajSekundaDziesatek)
+        val buttonOdejmijSekundaJednosci = view.findViewById<Button>(R.id.odejmijSekundeJednosci)
+        val buttonOdejmijSekundaDziesiatek = view.findViewById<Button>(R.id.odejmijSekundeDziesiatki)
+        val buttonDodajMinutaJednosci = view.findViewById<Button>(R.id.buttonDodajMinutaJednosci)
+        val buttonDodajMinutaDziesiatek = view.findViewById<Button>(R.id.buttondodajMinutaDziesiatek)
+        val buttonOdejmijMinutaJednosci = view.findViewById<Button>(R.id.buttonOdejmijMinuteJednostek)
+        val buttonOdejmijMinutaDziesiatek = view.findViewById<Button>(R.id.buttonOdejmijMinuteDziesiatek)
 
         val minuty = arguments?.getInt("minuty") ?: 0
         val sekundy = arguments?.getInt("sekundy") ?: 0
 
         czasStartu = (minuty * 60 + sekundy) * 1000L
-        pozostalyCzas = czasStartu
-        minutnikTextView.text = formatTime(pozostalyCzas)
+        timer = Timer(requireContext(), czasStartu, onTick = { czas -> minutnikTextView.text = czas })
+
+        minutnikTextView.text = timer?.formatTime(czasStartu)
 
         buttonStart.setOnClickListener {
-            if (!isRunning && pozostalyCzas>999) {
-                startMinutnik(pozostalyCzas, minutnikTextView)
-            }
+            timer?.startMinutnik()
         }
 
         buttonStop.setOnClickListener {
-            minutnik?.cancel()
-            isRunning = false
-            pozostalyCzas = 0L
-            minutnikTextView.text = formatTime(pozostalyCzas)
-            zatrzymajDzwiek()
+            timer?.stop()
+            minutnikTextView.text = timer?.formatTime(czasStartu)
         }
 
         buttonPauza.setOnClickListener {
-            if (isRunning) {
-                minutnik?.cancel()
-                isRunning = false
-            }
+            timer?.pauza()
+        }
+
+        buttonDodajSekundaJednosci.setOnClickListener{
+            timer?.dodajSekundaJednosci()
+        }
+
+        buttonDodajSekundaDziesiatek.setOnClickListener {
+            timer?.dodajSekundaDziesiatek()
+        }
+
+        buttonOdejmijSekundaJednosci.setOnClickListener {
+            timer?.odejmijSekundaJednosci()
+        }
+
+        buttonOdejmijSekundaDziesiatek.setOnClickListener {
+            timer?.odejmijSekundaDziesiatek()
+        }
+
+        buttonDodajMinutaJednosci.setOnClickListener{
+            timer?.dodajMinutyJednosci()
+        }
+
+        buttonDodajMinutaDziesiatek.setOnClickListener {
+            timer?.dodajMinutyDziesiatki()
+        }
+
+        buttonOdejmijMinutaJednosci.setOnClickListener {
+            timer?.odejmijMinutaJednosci()
+        }
+
+        buttonOdejmijMinutaDziesiatek.setOnClickListener {
+            timer?.odejmijMinutaDziesiatek()
         }
 
         konfiguracja.setOnClickListener {
@@ -62,45 +89,8 @@ class fragmentMinutnik : Fragment() {
         return view
     }
 
-    private fun startMinutnik(czasMilisekundy: Long, minutnikTextView: TextView) {
-        minutnik = object : CountDownTimer(czasMilisekundy, 1000) {
-            override fun onTick(pozostalyCzasMS: Long) {
-                pozostalyCzas = pozostalyCzasMS
-                minutnikTextView.text = formatTime(pozostalyCzas)
-                Log.d("TAG",pozostalyCzas.toString())
-            }
-
-            override fun onFinish() {
-                isRunning = false
-                pozostalyCzas = 0L
-                minutnikTextView.text = formatTime(pozostalyCzas)
-                odtworzDzwiek()
-            }
-        }.start()
-        isRunning = true
-    }
-
-    private fun formatTime(czasMilisekundy: Long): String {
-        val minuty = (czasMilisekundy/1000)/60
-        val sekundy = (czasMilisekundy/1000)%60
-        return String.format("%02d:%02d", minuty, sekundy)
-    }
-
-    private fun odtworzDzwiek(){
-        zatrzymajDzwiek()
-        dzwiekKonca = MediaPlayer.create(context, R.raw.alarm)
-        dzwiekKonca?.setVolume(0.1f, 0.1f)
-        dzwiekKonca?.start()
-    }
-
-    private fun zatrzymajDzwiek(){
-        dzwiekKonca?.stop()
-        dzwiekKonca?.release()
-        dzwiekKonca = null
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
-        zatrzymajDzwiek()
+        timer?.stop()
     }
 }
